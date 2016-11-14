b14641dd36eb52adada93e9bd2b29d3bc8f86062
danthelion <danivgy@gmail.com>
2016-11-14 15:39:24 +0100

    changed tests

--- fizzbuzz-test.py
+++ fizzbuzz-test.py
@@ -11,8 +11,7 @@ import pytest
         (15, 'FizzBuzz'),
         (16, 16),
         (30, 'FizzBuzz'),
-        (4, 4),
-        (31, 'Valszeg nem jo')
+        (4, 4)
     ]
 )
 def test_fizzbuzz(number, word):
